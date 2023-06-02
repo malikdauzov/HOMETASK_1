862b76257d3dadb383efa3b765430a2561d8fc8e
Malik <astemirmail@gmail.com>
2023-06-02 13:26:30 +0600

    add Hometask 3 i

---
+++ Hometask_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Number = 0;
+Console.WriteLine("Get number");
+Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Number is even");
+}
+else
+{
+    Console.WriteLine("Number is odd ");
+}
+

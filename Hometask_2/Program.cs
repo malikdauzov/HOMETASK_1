a3257b083501b9dfee77c36f77afa6e638b59f90
Malik <astemirmail@gmail.com>
2023-06-02 13:09:58 +0600

    add Hometask 2i c# Please enter the commit message for your changes. Lines starting

---
+++ Hometask_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int FirstNumber = 0;
+int SecondNumber = 0;
+int ThirdNumber = 0;
+Console.WriteLine("Get first numbers");
+FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Get second numbers");
+SecondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Get third numbers");
+ThirdNumber = Convert.ToInt32(Console.ReadLine());
+if (FirstNumber > SecondNumber && FirstNumber > ThirdNumber)
+    {
+    Console.WriteLine("Greater number is first: " + FirstNumber);
+    }
+else if (SecondNumber > ThirdNumber)
+{
+Console.WriteLine("Greater number  is second: " + SecondNumber);
+}
+else
+{
+Console.WriteLine("Greater number  is third: " + ThirdNumber);
+}

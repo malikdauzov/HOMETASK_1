7d36d1c16e4d67736e7e988f66da341ffa02296b
Malik <astemirmail@gmail.com>
2023-06-02 12:07:46 +0600

    add Hometask 1 i :wq i

---
+++ Hometask_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+int FirstNumber  = 0;
+int SecondNumber  = 0;
+Console.WriteLine("Get first numbers");
+FirstNumber = Convert.ToInt32(Console.ReadLine()); //int.Parse()
+Console.WriteLine("Get Second numbers");
+FirstNumber = Convert.ToInt32(Console.ReadLine());
+if(FirstNumber>SecondNumber)
+{
+    Console.WriteLine("Greater number:" + FirstNumber);
+    Console.WriteLine("Smaller number:" + SecondNumber);
+}
+else
+{
+Console.WriteLine("Greater number:" + FirstNumber);
+Console.WriteLine("Smaller number:" + SecondNumber);
+}

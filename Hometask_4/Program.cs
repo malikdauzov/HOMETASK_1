625e3c4312203b73cc5282c68c8b5c39be07e7ae
Malik <astemirmail@gmail.com>
2023-06-02 13:33:54 +0600

    add Hometask 4 i

---
+++ Hometask_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Number = 0;
+int X = 1;
+Console.WriteLine("Get number");
+Number = Convert.ToInt32(Console.ReadLine());
+while (X < Number)
+{
+if(X % 2 == 0)
+{
+    Console.WriteLine(X);
+}
+X++;
+}

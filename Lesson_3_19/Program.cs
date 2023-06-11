e2aab7ff5d759d9e7829d1c2f6b487bc53d92218
Malik <astemirmail@gmail.com>
2023-06-11 23:30:27 +0600

    Lesson_3_19

---
+++ Lesson_3_19/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine("Да, число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Нет, число не является палиндромом.");
+        }
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        string numberStr = number.ToString();
+        int length = numberStr.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (numberStr[i] != numberStr[length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

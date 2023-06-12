1f429055534f67bd6faed74909a8245f29eeefcb
Malik <astemirmail@gmail.com>
2023-06-12 18:45:51 +0600

    Lesson_4.2

---
+++ Lesson_4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+
+int number1 = 4;
+int number2 = 5;
+int number3 = 2;
+//int result = number + number;
+double result = (number1 + number2 + number3);
+
+
+Console.WriteLine( result );

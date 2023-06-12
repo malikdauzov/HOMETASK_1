ed026d002e538a7edfda4e4f0f1bcbbbb14860c8
Malik <astemirmail@gmail.com>
2023-06-12 14:46:19 +0600

    Lesson_4.1

---
+++ Lesson_4.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int A = 2; // Число
+
+int B = 4; // Степень
+
+double result = 1; // 1
+
+for (int i = 1; i <= B; i++)
+{
+    result *= A; // result = result * A
+}
+
+Console.WriteLine(result);

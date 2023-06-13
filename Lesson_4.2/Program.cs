427960f30dbe94c285d44cde61887656db9447f6
Malik <astemirmail@gmail.com>
2023-06-13 09:56:50 +0600

    Lesson_4.2

--- Lesson_4.2/Program.cs
+++ Lesson_4.2/Program.cs
@@ -4,12 +4,16 @@
 //9012 -> 12
 
 
-
-int number1 = 4;
-int number2 = 5;
-int number3 = 2;
+Console.Write("Введите числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+//int number1 = 4;
+//int number2 = 5;
+//int number3 = 2;
+int result = number1 + number2 + number3;
 //int result = number + number;
-double result = (number1 + number2 + number3);
+//double result = (number1 + number2 + number3);
 
 
 Console.WriteLine( result );
